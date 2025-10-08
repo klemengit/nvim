6411f4074a813db3b1fa7226b25db4798162e150
Klemen Zaletelj <klemen.zaletelj@fs.uni-lj.si>
2025-10-08 15:07:44 +0200

    comment is ctrl+shift+/ (7 on slo keyboard), same as in vsc

---
+++ lua/plugins/comment-nvim.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    "numToStr/Comment.nvim",
+    keys = {
+      { "<C-S-/>", "gcc", desc = "Toggle comment line", remap = true, mode = "n" },
+      { "<C-S-/>", "gc", desc = "Toggle comment", remap = true, mode = "v" },
+    },
+  },
+}
