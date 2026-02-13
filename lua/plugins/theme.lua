a4594dad3c75f461db02a7fedb00f23a4fd33027
Klemen Zaletelj <klemen.zaletelj@fs.uni-lj.si>
2026-02-13 09:21:14 +0100

    Added comment on lualine when in light mode (NVIM_LIGHT=1)

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -5,4 +5,17 @@ return {
       colorscheme = "tokyonight",
     },
   },
+  {
+    "nvim-lualine/lualine.nvim",
+    opts = function(_, opts)
+      if vim.g.lightweight then
+        table.insert(opts.sections.lualine_x, 1, {
+          function()
+            return "NVIM LIGHT"
+          end,
+          color = { fg = "#ff9e64" },
+        })
+      end
+    end,
+  },
 }
