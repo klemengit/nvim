3fe3b3d2147e9c12285648bc85982ee140d78eab
Klemen Zaletelj <klemen.zaletelj@fs.uni-lj.si>
2026-03-13 09:51:34 +0100

    updated colors for comments and molten.nvim output

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,5 +1,16 @@
 return {
   {
+    "folke/tokyonight.nvim",
+    opts = {
+      on_highlights = function(hl, c)
+        -- Brighter comments (default is ~#565f89, bumped up for readability)
+        hl.Comment = { fg = "#7c87b8", italic = true }
+        -- Molten virtual text output: warm yellow, distinct from blue comments
+        hl.MoltenVirtualText = { fg = "#73daca" }
+      end,
+    },
+  },
+  {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "tokyonight",
